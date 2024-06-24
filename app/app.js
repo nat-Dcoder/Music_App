92bc37a2f7e8741f0a68ea27f296b9c675ec2eba
nat-Dcoder <nj433487@gmail.com>
2024-06-24 14:37:28 +0000

    first commit

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
 
-// Serve static files from the 'public' folder
+// Serving static files from the 'public' folder
 app.use(express.static('../public'));
 
 app.listen(PORT, () => {
